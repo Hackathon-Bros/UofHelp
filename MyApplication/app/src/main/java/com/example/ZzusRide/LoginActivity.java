b405e39562841c7fc9b3fc19b6fb5c91319fd374
Brandon-Davison <Brandon_Davison@outlook.com>
2018-03-03 19:06:05 -0800

    Added drawables and updated login UI

--- MyApplication/app/src/main/java/com/example/ZzusRide/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/ZzusRide/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.ZzusRide;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,6 +47,11 @@ public class LoginActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        try {
+            this.getSupportActionBar().hide();
+        } catch (Exception e) {
+
+        }
         setContentView(R.layout.activity_login);
 
         progress = (ProgressBar) findViewById(R.id.progress);
